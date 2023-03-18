1af297b5c007e159bc7b62ea82cb5c187c315a11
aliceakesson <alicek732@gmail.com>
2023-03-18 18:15:20 +0100

    First part of hint system

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var listened = 0;
+var listened = 1;
 
 var audio = new Audio("stormvind.mp3");
+var song = "Fångad av en stormvind - Carola";
 var time = 0; 
 
 function isPlaying() { return !audio.paused; }
@@ -9,9 +10,10 @@ const lightColor = "#eee";
 const darkColor = "#666";
 const redColor = "red";
 const greenColor = "green";
+const bgColor = "#111";
 
 const timeWidth = 700; 
-const endTime = 16 * 1000; 
+const endTime = 16; 
 
 var clearButton = document.querySelector('#textfield i:last-child');
 var playButton = document.querySelector('#play i');
@@ -23,43 +25,45 @@ var textfield = document.querySelector("#textfield textarea");
 
 var timeObject = document.getElementById('currentTime');
 
-setInterval(run, 1);
-document.querySelector("#play p:last-child").innerHTML = "0:" + (endTime / 1000);
+setInterval(run, 10);
+document.querySelector("#play p:last-child").innerHTML = "0:" + endTime;
 
 function run() {
     if(isPlaying()) {
-        if(time < endTime) {
+        var maxWidth = 0;
+        for(let i = 2; i <= listened + 1; i++) {
+            var part = document.querySelector("#time-parts div:nth-child(" + i + ")");
+            console.log(part.style.width);
+            maxWidth += part.style.width;
+        }
+
+        //console.log(maxWidth);
+
+        if(time < endTime * 100) {
             time += 1; 
-            var newWidth = (time / endTime) * 100;
+            var newWidth = (time / (endTime * 100)) * 100;
             timeObject.style.width = newWidth + "%";
+        } else if(time == endTime * 100) {
+            pause();
         }
     }
 }
 
-clearButton.addEventListener('click', function() {
-    textfield.value = "";
-});
+clearButton.addEventListener('click', clear);
 
 playButton.addEventListener('click', function() {
-    if(playButton.classList.contains("fa-play")) {
-        playButton.classList.remove("fa-play");
-        playButton.classList.add("fa-pause");
-        audio.currentTime = 0; 
-        time = 0; 
-        audio.play();
-    } else {
-        playButton.classList.remove("fa-pause");
-        playButton.classList.add("fa-play");
-        audio.pause();
-        timeObject.style.width = "0";
-    }
+    if(playButton.classList.contains("fa-play"))
+        play()
+    else 
+        pause()
 });
 
-skipButton.addEventListener('click', function() {
+skipButton.addEventListener('click', revealMore);
 
+submitButton.addEventListener('click', function() {
+    var song = textfield.value; 
+    submit(song);
 });
-
-submitButton.addEventListener('click', submit);
 
 textfield.addEventListener('focus', function() {
     textDiv.style.borderColor = greenColor;
@@ -68,6 +72,44 @@ textfield.addEventListener('blur', function() {
     textDiv.style.borderColor = lightColor;
 });
 
-function submit() {
+function submit(song) {
+    if(song != this.song) {
+        if(song != "") {
+            clear();
+            revealMore();
+        }
+    }
+    else {
+        console.log("Du gissade rätt!");
+    }
+}
 
+function play() {
+    playButton.classList.remove("fa-play");
+    playButton.classList.add("fa-pause");
+    audio.currentTime = 0; 
+    time = 0; 
+    audio.play();
+}
+function pause() {
+    playButton.classList.remove("fa-pause");
+    playButton.classList.add("fa-play");
+    audio.pause();
+    timeObject.style.width = "0";
+}
+
+function revealMore() {
+    if(listened < 6) {
+        listened += 1;
+
+        var part = document.querySelector("#time-parts div:nth-child(" + (listened + 1) + ")");
+        part.style.backgroundColor = darkColor;
+
+        var prePart = document.querySelector("#time-parts div:nth-child(" + listened + ")");
+        prePart.style.borderColor = bgColor; 
+    }
+}
+
+function clear() {
+    textfield.value = "";
 }
